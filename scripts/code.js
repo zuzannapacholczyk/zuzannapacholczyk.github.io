34433e860a2c9a2334e0a3e3ae4f484ec493730f
Zuzanna Pacholczyk <zuzannapacholczyk@wp.pl>
2016-07-19 10:38:29 +0200

    arrows added

--- scripts/code.js
+++ scripts/code.js
@@ -1,13 +1,39 @@
 var main = function() {
     $('.gallery_pic').click(function() {
         var imgSrc = $(this).attr('src');
-        $('.gallery_pic_zoom_chosen').remove();
-        $('.gallery_pic_zoom_content').prepend($('<img>',{class:'gallery_pic_zoom_chosen',src: imgSrc}));
+        $(this).addClass('curr_pic');
+        $('.gallery_pic_zoom_chosen').attr("src", imgSrc);
         $('.gallery_pic_zoom').show();
     });
 
     $('.gallery_pic_zoom_close').click(function() {
         $('.gallery_pic_zoom').hide();
+    });
+    
+    $('.gallery_pic_zoom_prev').click(function() {
+        var currPic = $('.curr_pic');
+        var prevPic = currPic.prev();
+        
+        if(prevPic.length == 0) {
+            prevPic = $('.gallery_pic').last();
+        }
+        
+        currPic.removeClass('curr_pic');
+        prevPic.addClass('curr_pic');
+        $('.gallery_pic_zoom_chosen').attr("src", $('.curr_pic').attr('src'));
+    });
+    
+    $('.gallery_pic_zoom_next').click(function() {
+        var currPic = $('.curr_pic');
+        var nextPic = currPic.next();
+        
+        if(nextPic.length == 0) {
+            nextPic = $('.gallery_pic').first();
+        }
+        
+        currPic.removeClass('curr_pic');
+        nextPic.addClass('curr_pic');
+        $('.gallery_pic_zoom_chosen').attr("src", $('.curr_pic').attr('src'));
     });
 
 };
